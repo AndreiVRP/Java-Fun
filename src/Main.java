e2a82ff98f84842d9a7da4fc6cda248031a72f1f
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-11-15 09:29:47 +0300

    Section 3, lessons 8-11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Car newCar = new Car(15.5, "2375AA-1", Color.WHITE, true);
+        System.out.println("The new car's license number is " + newCar.licensePlate);
+        newCar.changeColor(Color.RED);
+        System.out.println(newCar.paintColor);
+
     }
 }
